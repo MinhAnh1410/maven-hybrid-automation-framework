be74e4699c81d9e115b53f7e0f614e50bb770eeb
MinhAnh1410 <minhanh1410k54@gmail.com>
2025-11-02 17:34:29 +0700

    0211

---
+++ src/test/java/employee/Employee_01_CreateEmployee.java
@@ -0,0 +1,31 @@
+package employee;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Employee_01_CreateEmployee {
+    @BeforeClass
+    public void beforeClass() {
+
+    }
+
+    @Test
+    public void Employee_01_NewEmployee(){
+
+    }
+
+    @Test
+    public void Employee_02_NewEmployee(){
+
+    }
+
+    @Test
+    public void Employee_03_NewEmployee(){
+
+    }
+
+    @Test
+    public void Employee_04_NewEmployee(){
+
+    }
+}
