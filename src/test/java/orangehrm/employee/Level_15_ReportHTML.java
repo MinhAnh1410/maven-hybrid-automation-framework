d6654a80cc1cf927eeb06685422f7f866a504e40
MinhAnh1410 <minhanh1410k54@gmail.com>
2026-05-27 23:38:40 +0700

    Extent Report ver 2

---
+++ src/test/java/orangehrm/employee/Level_15_ReportHTML.java
@@ -0,0 +1,159 @@
+package orangehrm.employee;
+
+import com.relevantcodes.extentreports.LogStatus;
+import core.BasePage;
+import core.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.PageGeneration;
+import pageObjects.PageGenerator;
+import pageObjects.orangehrm.AddEmployeePO;
+import pageObjects.orangehrm.DashboardPO;
+import pageObjects.orangehrm.EmployeeListPO;
+import pageObjects.orangehrm.LoginPO;
+import pageObjects.orangehrm.myInfo.ContactDetailPO;
+import pageObjects.orangehrm.myInfo.DependentPO;
+import pageObjects.orangehrm.myInfo.JobPO;
+import pageObjects.orangehrm.myInfo.PersonalDetailPO;
+import reportConfigs.ExtentManager;
+
+import java.lang.reflect.Method;
+
+public class Level_15_ReportHTML extends BaseTest {
+    private WebDriver driver;
+    private BasePage basePage = BasePage.getBasePage();
+    private String adminUsername, adminPassword;
+    private String firstName, lastName, fullName, emailAddress, password, employeeID, passportNumber, passportComment;
+
+
+    @Parameters ({"Browser","Url"})
+    @BeforeClass
+    public void beforeClass(String browserName, String url) {
+        System.out.println("Run on browser: " + browserName);
+        driver = getBrowserDriver(browserName, url);
+
+        firstName = "Peter";
+        lastName = "Crouch";
+        adminUsername = "Admin";
+        adminPassword = "admin123";
+        fullName = firstName + " " + lastName;
+        emailAddress = "peter" + getRandomNumber() + "@hotmail.com";
+        password = "Testing111###";
+        passportNumber = "43176122";
+        passportComment = "Assigned Imigration\nRecords";
+
+        loginPage = PageGeneration.getPage(LoginPO.class, driver);
+    }
+
+    @Test
+    public void Employee_01_NewEmployee(Method method) {
+
+        ExtentManager.startTest(method.getName(), "Employee_01_NewEmployee");
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 01: Enter to Username textbox with value is '"+ adminUsername + "'");
+        loginPage.enterToUsernameTextbox(adminUsername);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 02: Enter to Password textbox with value is '"+ adminPassword + "'");
+        loginPage.enterToPasswordTextbox(adminPassword);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 03: Click to Login button");
+        dashboardPage = loginPage.clickToLoginButton();
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 04: Verify loading icon invisible");
+        Assert.assertTrue(dashboardPage.isLoadingIconDisappear(driver));
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 05: Verify Dashboard header is displayed");
+        Assert.assertFalse(dashboardPage.isDashboardHeaderDisplayed()); // lỗi
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 06: Click to PIM menu link");
+        employeeListPage = dashboardPage.clickToPIMModule();
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 07: Verify loading icon invisible");
+        Assert.assertTrue(employeeListPage.isLoadingIconDisappear(driver));
+
+        ExtentManager.getTest().log(LogStatus.INFO, "NewEmployee - Step 08: Verify PIM header is displayed");
+        Assert.assertFalse(employeeListPage.isPIMHeaderDisplayed()); // lỗi
+
+        ExtentManager.endTest();
+    }
+
+    @Test
+    public void Employee_02_ViewEmployee(Method method) {
+
+        ExtentManager.startTest(method.getName(), "Employee_02_ViewEmployee");
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 01: Click to Employee List menu link");
+        addEmployeePage = employeeListPage.clickToAddEmployeeButton();
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 02: Verify loading icon invisible");
+        Assert.assertTrue(addEmployeePage.isLoadingIconDisappear(driver));
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 03: Enter to First name textbox with value is '"+ firstName + "'");
+        addEmployeePage.enterToFirstNameTextbox(firstName);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 04: Enter to First name textbox with value is '"+ lastName + "'");
+        addEmployeePage.enterToLastNameTextbox(lastName);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 05: Get Employee ID value");
+        employeeID = addEmployeePage.getEmployeeIDValue();
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 06: Click to Create Login Details checkbox");
+        addEmployeePage.clickToCreateLoginDetailCheckbox();
+        addEmployeePage.sleepInSecond(2);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 07: Enter to Email textbox with value is '"+ emailAddress + "'");
+        addEmployeePage.enterToUsernameTextbox(emailAddress);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 08: Enter to Password textbox with value is '"+ password + "'");
+        addEmployeePage.enterToPasswordTextbox(password);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 09: Enter to Confirm Password textbox with value is '"+ password + "'");
+        addEmployeePage.enterToConfirmPasswordTextbox(password);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 10: Click to Save button");
+        addEmployeePage.clickToSaveButton();
+        addEmployeePage.sleepInSecond(2);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 11: Verify Successfully Saved message is displayed");
+        Assert.assertFalse(addEmployeePage.isSuccessfullySaveMessageDisplayed()); // lỗi
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 12: Navigate to Personal Details page");
+        personalDetailPage = PageGenerator.getPersonalDetailPage(driver);
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 13: Verify loading icon invisible");
+        Assert.assertTrue(personalDetailPage.isLoadingIconDisappear(driver));
+        Assert.assertTrue(personalDetailPage.isLoadingIconDisappear(driver));
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 14: Verify First name textbox value is '" + firstName + "'");
+        Assert.assertNotEquals(personalDetailPage.getFirstnameTextboxValue(), firstName); // lỗi
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 15: Verify Last name textbox value is '" + lastName + "'");
+        Assert.assertNotEquals(personalDetailPage.getLastnameTextboxValue(), lastName); // lỗi
+
+        ExtentManager.getTest().log(LogStatus.INFO, "ViewEmployee - Step 16: Verify Employee ID textbox value is '" + employeeID + "'");
+        Assert.assertNotEquals(personalDetailPage.getEmployeeTextboxValue(), employeeID);
+
+        ExtentManager.endTest();
+    }
+
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    private LoginPO loginPage;
+    private DashboardPO dashboardPage;
+    private EmployeeListPO employeeListPage;
+    private PersonalDetailPO personalDetailPage;
+    private AddEmployeePO addEmployeePage;
+    private ContactDetailPO contactDetailPage;
+    private JobPO jobPage;
+    private DependentPO dependentPage;
+
+}
